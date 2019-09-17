a094036b9a32cf3373430096c666edbc68e799e3
Nick Burrin <NickBurrin18@gmail.com>
2019-09-16 20:44:32 -0700

    Updates to home page

--- app/views/Index.js
+++ app/views/Index.js
@@ -12,14 +12,14 @@ const Index = () => (
           <p>A beautiful, responsive, react app written with modern Javascript.</p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more{/* <Link to="/about">about me</Link> */},
+      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         {/* <Link to="/projects">projects</Link>, {' '} */}
         {/* view <Link to="/stats">site statistics</Link>, {' '} */}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/nickburrin/personal-site">here</a>.</p>
+      <p> Source code available <a href="https://github.com/nickburrin/personal-site">here</a>.</p>
     </article>
   </Main>
 );
